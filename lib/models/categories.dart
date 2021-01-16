c81be4bdfb50f6845b867eea747d26d5a94d10e9
Meshrif <meshrif@Meshrifs-Mac-Pro.local>
2021-01-16 07:34:04 +0200

    edit home page

---
+++ lib/models/categories.dart
@@ -0,0 +1,14 @@
+class Category {
+  final String cato, images;
+
+  Category({this.cato, this.images});
+}
+
+List<Category> categories = [
+  Category(cato: "General Knowledge", images: "images/catogories/gKnowloage.png"),
+  Category(cato: "Books", images: "images/catogories/books.png"),
+  Category(cato: "Film", images: "images/catogories/film.png"),
+  Category(cato: "Music", images: "images/catogories/music.png"),
+  Category(cato: "Football", images: "images/catogories/football.png"),
+  Category(cato: "Bobybuilding", images: "images/catogories/bodybuilding.png"),
+];
